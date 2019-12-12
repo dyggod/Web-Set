4cae8b72c5bdc0b40add4bdfd6ec1cbde89c27e1
dyggod <dyggod@163.com>
2019-12-12 11:16:06 +0800

    add router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './page/home/home.component';
+import {ChatRoomComponent } from './page/chat-room/chat-room.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", redirectTo: "/home", pathMatch: "full"},
+  {path: "home", component: HomeComponent},
+  {path: "chatRoom", component: ChatRoomComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChatRoomComponent } from './page/chat-room/chat-room.component';
+import { HomeComponent } from './page/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatRoomComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+  ],
+  entryComponents: [
+    AppComponent,
+    ChatRoomComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
